bae4a68e0e555585596ebdb253f4b1f6f03bd87a
Landic <danil.volkov.235@gmail.com>
2023-02-13 11:05:42 +0200

    Добавил дз за 10.02.2023

---
+++ Volkov_HW_10_NET/Volkov_HW_10_NET/Book.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volkov_HW_10_NET
+{
+    internal class Book
+    {
+        private string name;
+        private string autor;
+        private int page;
+        public Book()
+        {
+            name = string.Empty;
+            autor= string.Empty;
+            page = 0;
+        }
+        public Book(string name, string autor, int page)
+        {
+            this.name = name;
+            this.autor = autor;
+            this.page = page;
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Enter name book");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter autor");
+            autor = Console.ReadLine();
+            Console.WriteLine("Enter page");
+            page = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public override string ToString()
+        {
+            return $"Name book -> {name}\nAutor -> {autor}\nPage -> {page}\n";
+        }
+
+        public string Name
+        {
+            get;set;
+        }
+    }
+}
---
+++ Volkov_HW_10_NET/Volkov_HW_10_NET/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volkov_HW_10_NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book obj = new Book();
+            obj.Input();
+            Console.WriteLine(obj);
+            List_Book obj2 = new List_Book();
+            if(obj2.Find(obj, "jack") == true)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+            Console.WriteLine(obj);
+        }
+    }
+}

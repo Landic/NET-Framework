02e5b0c1d5a92d4dbd050b483eabe2eb1032ac94
Landic <danil.volkov.235@gmail.com>
2023-02-20 12:42:12 +0200

    Добавил дз за 20.02.2023

---
+++ Volkov_HW_15_NET/Volkov_HW_15_NET/CreditCard.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volkov_HW_15_NET
+{
+    internal class CreditCard
+    {
+        private string NS;
+        private string date;
+        private int PIN;
+        private int CreditLimit;
+        private int balance;
+        List<MyDelegate> list = new List<MyDelegate>();
+        private string history;
+
+        public event MyDelegate ev
+        {
+            // Используем аксессоры событий
+            add
+            {
+                list.Add(value);
+            }
+
+            remove
+            {
+                list.Remove(value);
+            }
+        }
+
+        public CreditCard()
+        {
+            NS = string.Empty;
+            date = string.Empty;
+            PIN = 0;
+            CreditLimit = 0;
+            balance = 0;
+            history = string.Empty;
+        }
+        
+        public CreditCard(string NS, string date, int PIN, int CreditLimit, int balance)
+        {
+            this.NS = NS;
+            this.date = date;
+            this.PIN = PIN;
+            this.CreditLimit = CreditLimit;
+            this.balance = balance;
+        }
+
+        public void Init()
+        {
+            Console.WriteLine("Enter name surname");
+            NS = Console.ReadLine();
+            Console.WriteLine("Enter date end card MM/YY");
+            date = Console.ReadLine();
+            Console.WriteLine("Enter PIN code");
+            PIN = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter credit limit");
+            CreditLimit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter balance");
+            balance = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public override string ToString()
+        {
+            return $"NS -> {NS}\nDate end -> {date}\nPIN-code -> {PIN}\nCredit limit -> {CreditLimit}$\nBalance -> {balance}$";
+        }
+
+        public void Event()
+        {
+            if(list.Count != 0)
+            {
+                foreach (MyDelegate i in list)
+                    i();
+            }
+        }
+
+        public void AddBalance()
+        {
+            int add;
+            Console.WriteLine("Enter summ to add balance");
+            add = Convert.ToInt32(Console.ReadLine());
+            balance += add;
+            history += "Add to balance -> " + add;
+        }
+
+        public void History()
+        {
+            Console.WriteLine(history + "\n -10$ - spent on food\n -100$ - spent on medicin");
+        }
+
+        public void Credit()
+        {
+            Console.WriteLine("Credit start!");
+        }
+
+        public void ReachingTheSum()
+        {
+            int reaching;
+            Console.WriteLine("Enter reaching the sum");
+            reaching = int.Parse(Console.ReadLine());
+            if(balance > reaching)
+            {
+                Console.WriteLine("Summ reaching");
+            }
+            else
+            {
+                Console.WriteLine("Sum not reaching");
+            }
+        }
+
+        public void ChangePIN()
+        {
+            int newpin = 0;
+            Console.WriteLine("Enter old PIN-code");
+            newpin = Convert.ToInt32(Console.ReadLine());
+            if(newpin != PIN)
+            {
+                throw new Exception("You enter wrong PIN");
+            }
+            else
+            {
+                Console.WriteLine("Enter new PIN-code");
+                PIN = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ Volkov_HW_15_NET/Volkov_HW_15_NET/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volkov_HW_15_NET
+{
+    delegate void MyDelegate();
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditCard obj = new CreditCard("Barak Obama", "10.25", 4235, 1000, 21000);
+            int choose = 0;
+            while (true)
+            {
+                Console.WriteLine("1. Info card\n2. Add to the balance\n3. History\n4. Start credit\n5. Reaching summ\n6. Change PIN-Code");
+                choose = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                switch (choose)
+                {
+                    case 1:
+                        Console.WriteLine(obj);
+                        Console.WriteLine("1. Back");
+                        choose = Convert.ToInt32(Console.ReadLine());
+                        Console.Clear();
+                        break;
+                    case 2:
+                        obj.ev += new MyDelegate(obj.AddBalance);
+                        obj.Event();
+                        obj.ev -= obj.AddBalance;
+                        Console.Clear();
+                        break;
+                    case 3:
+                        obj.ev += new MyDelegate(obj.History);
+                        obj.Event();
+                        obj.ev -= obj.AddBalance;
+                        Console.WriteLine("1. Back");
+                        choose = Convert.ToInt32(Console.ReadLine());
+                        Console.Clear();
+                        break;
+                    case 4:
+                        obj.ev += new MyDelegate(obj.Credit);
+                        obj.Event();
+                        obj.ev -= obj.Credit;
+                        Console.WriteLine("1. Back");
+                        choose = Convert.ToInt32(Console.ReadLine());
+                        Console.Clear();
+                        break;
+                    case 5:
+                        obj.ev += new MyDelegate(obj.ReachingTheSum);
+                        obj.Event();
+                        obj.ev -= obj.ReachingTheSum;
+                        Console.WriteLine("1. Back");
+                        choose = Convert.ToInt32(Console.ReadLine());
+                        Console.Clear();
+                        break;
+                    case 6:
+                        obj.ev += new MyDelegate(obj.ChangePIN);
+                        obj.Event();
+                        obj.ev -= obj.ChangePIN;
+                        Console.WriteLine("1. Back");
+                        choose = Convert.ToInt32(Console.ReadLine());
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.WriteLine("Error choose");
+                        System.Threading.Thread.Sleep(1000);
+                        Console.Clear();
+                        break;
+
+                }
+            }
+        }
+    }
+}

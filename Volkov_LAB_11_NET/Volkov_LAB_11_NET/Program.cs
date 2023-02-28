b07e175e6ab3410e79a1fd89217ce1d3baa04d1b
Landic <danil.volkov.235@gmail.com>
2023-02-28 11:05:52 +0200

    Добавил лабу за 28.02.2023

---
+++ Volkov_LAB_11_NET/Volkov_LAB_11_NET/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volkov_LAB_11_NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

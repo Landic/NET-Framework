02b41b683af35b5093b1b1a3adf73d355822e6ae
Landic <danil.volkov.235@gmail.com>
2023-03-01 14:18:19 +0200

    Добавил дз за 01.03.2023

---
+++ Volkov_HW_21_NET/Volkov_HW_21_NET/Piece.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volkov_HW_21_NET
+{
+    internal class Piece : IDisposable
+    {
+        string namepiece;
+        string NS;
+        string genre;
+        int year;
+
+        public Piece()
+        {
+            namepiece= string.Empty;
+            NS = string.Empty;
+            genre = string.Empty;
+            year = 0;
+        }
+
+        public Piece(string namepiece, string nS, string genre, int year)
+        {
+            this.namepiece = namepiece;
+            NS = nS;
+            this.genre = genre;
+            this.year = year;
+        }
+
+        public override string ToString()
+        {
+            return $"Name piece -> {namepiece}\nName surname -> {NS}\nGenre -> {genre}\nYear -> {year}";
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose");
+        }
+
+        ~Piece()
+        {
+            Console.WriteLine("Finalize");
+        }
+    }
+}
---
+++ Volkov_HW_21_NET/Volkov_HW_21_NET/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volkov_HW_21_NET
+{
+    internal class Program
+    {
+        static void Task1()
+        {
+            Piece[] obj = new Piece[20];
+            for (int i = 0; i < 20; i++)
+            {
+                obj[i] = new Piece();
+            }
+        }
+
+        static void Task2()
+        {
+            Shop[] obj = new Shop[20];
+            for (int i = 0; i < 20; i++)
+                using (obj[i] = new Shop())
+                {
+
+                }
+        }
+
+        static void Task3()
+        {
+            Shop[] obj1 = new Shop[20];
+            for (int i = 0; i < 20; i++)
+                obj1[i] = new Shop();
+            Piece[] obj2 = new Piece[20];
+            for (int i = 0; i < 20; i++)
+                using (obj2[i] = new Piece())
+                {
+
+                }
+        }
+
+
+        static void Main(string[] args)
+        {
+            //Task1();
+            //Task2();
+            Task3();
+        }
+    }
+}
